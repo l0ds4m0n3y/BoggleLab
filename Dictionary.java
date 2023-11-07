59fa527012a8a67ff268d2e81c24b28b1c539b10
l0ds4m0n3y <103957548+l0ds4m0n3y@users.noreply.github.com>
2023-11-07 14:48:41 -0800

    Update Dictionary.java

--- Dictionary.java
+++ Dictionary.java
@@ -3,46 +3,43 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Dictionary{
+public class Dictionary {
   private static ArrayList<String> dictionary = new ArrayList<>();
-  private static File file = new File("Dictionary.txt");
-  
-  Dictionary(){
-    try { load(); } catch (Exception e) {}
+
+  Dictionary() {
+    try {
+      load();
+    } catch (Exception e) {
+    }
   }
 
-  public void load() throws FileNotFoundException{
+  public void load() throws FileNotFoundException {
+    File file = new File("Dictionary.txt");
     Scanner scan = new Scanner(file);
-    while(scan.hasNextLine()){
+
+    while (scan.hasNextLine()) {
       dictionary.add(scan.nextLine());
     }
-    System.out.println(dictionary.size());
-    // for(String s : dictionary){
-    //   System.out.println(s);
-    // }
     scan.close();
   }
-  public static Boolean isValidWord(String word, int start, int end){
+
+  public static Boolean isValidWord(String word, int start, int end) {
     int index = (start + end) / 2;
 
-    if(end <= start) return false;
+    if (end < start)
+      return false;
 
     String wordAtIndex = dictionary.get(index);
-    if(word.equals(wordAtIndex)){
+    if (word.equals(wordAtIndex)) {
       return true;
-    }else if(word.compareTo(wordAtIndex) < 0){
+    } else if (word.compareTo(wordAtIndex) < 0) {
       return isValidWord(word, start, index - 1);
-    }else{
+    } else {
       return isValidWord(word, index + 1, end);
     }
   }
 
-  public int getDictionarySize() {
+  public static int getDictionarySize() {
     return dictionary.size();
-  }
-  
-  public static void main(String[] args) {
-    Dictionary dict = new Dictionary();
-    System.out.println(Dictionary.isValidWord("nincompoop", 0, dict.getDictionarySize()));
   }
 }
